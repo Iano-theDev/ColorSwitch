e7e25f656031e04f9b2b31269959f68537f764e7
IAN KAMANDE <60983828+Iano-theDev@users.noreply.github.com>
2022-02-27 19:31:37 +0300

    Simple colors project is complete

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "blue", "white", "black"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
@@ -13,5 +13,5 @@ btn.addEventListener("click", function(){
 });
 
 function getRandomNumber() {
-    return Math.random();
+    return Math.floor(Math.random()*colors.length);
 }
